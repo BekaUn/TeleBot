498bdbd030106a10c5218f16911b81018157e1fb
BekaUn <bekayunusov948@gmail.com>
2023-08-30 14:39:25 +0500

    Alish gay

---
+++ src/main/java/com/project/wecare/WeCareApplication.java
@@ -0,0 +1,24 @@
+package com.project.wecare;
+
+import com.project.wecare.controller.TelegramBotController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+@Slf4j
+
+public class WeCareApplication {
+
+    public static void main(String[] args) throws TelegramApiException {
+        SpringApplication.run(WeCareApplication.class, args);
+
+    }
+
+}
