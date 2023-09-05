c19026f5e2d7e156016151fe56614006ba239041
AkhmatovSardor <sardor.akhmatov0969@gmail.com>
2023-09-05 19:07:04 +0500

    update bot

--- src/main/java/com/project/wecare/util/Uz.java
+++ src/main/java/com/project/wecare/util/Uz.java
@@ -1,7 +1,5 @@
 package com.project.wecare.util;
 
-import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Fetch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
